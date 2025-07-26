bc53d521f71f99426bbb692ad33c9640d5e4d329
Jeddawy <ielgeddawy@gmail.com>
2025-07-26 20:37:28 +0300

    [Create] Country Repository to be responsible for countries fetching and data handling

---
+++ CountryList/Repository/CountriesRepository.swift
@@ -0,0 +1,43 @@
+//
+//  CountriesRepository.swift
+//  CountryList
+//
+//  Created by Ibrahim El-geddawy on 26/07/2025.
+//
+
+import Foundation
+
+protocol CountryRepository {
+    func fetchCountry(name: String) async throws -> Country
+    func searchCountries(query: String) async throws -> [Country]
+}
+
+class CountriesRepository: CountryRepository {
+    
+    private let apiClient = URLSessionAPIClient<CountryEndpoint>()
+    
+    func fetchCountry(name: String) async throws -> Country {
+        let details: CountryDetails = try await apiClient.request(.fetchCountry(name: name))
+        
+        return Country(
+            imageUrl: details.flags?.png ?? "",
+            name: details.name ?? "",
+            capital: details.capital ?? "",
+            currency: ""
+        )
+    }
+    
+    func searchCountries(query: String) async throws -> [Country] {
+        let details: [CountryDetails] = try await apiClient.request(.searchCountry(name: query))
+        
+        return details.map {
+            Country(
+                imageUrl: $0.flags?.png ?? "",
+                name: $0.name ?? "",
+                capital:  "",
+                currency:  ""
+            )
+        }
+    }
+    
+}
