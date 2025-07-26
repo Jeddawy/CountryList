8c2e97081c4b37adc13fd3f2233f8d11469d5621
Jeddawy <ielgeddawy@gmail.com>
2025-07-26 12:36:37 +0300

    Initial Commit

---
+++ CountryList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountryList
+//
+//  Created by Ibrahim El-geddawy on 26/07/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountryList/CountryListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryListApp.swift
+//  CountryList
+//
+//  Created by Ibrahim El-geddawy on 26/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct CountryListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
